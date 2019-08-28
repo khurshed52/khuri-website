5c120bc84b5a64a41263b7c3f21ce058a5864ac6
khurshed52 <khurshedkhan52@gmail.com>
2019-08-28 09:20:12 +0400

    routing fade animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from './shared/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations:[fadeAnimation]
 })
 export class AppComponent implements OnInit {
   title = 'khuri-website';
   ngOnInit(){
+  }
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
 }
