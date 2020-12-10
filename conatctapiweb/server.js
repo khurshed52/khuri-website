0d0b7511af90ffce5ea0d7b343e9345345e6f12d
khurshed khan <khurshed.khan@Gantner.me>
2020-12-10 15:30:01 +0400

    mongoose changes

--- conatctapiweb/server.js
+++ conatctapiweb/server.js
@@ -2,31 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: 'false'})); 
-const routes = require('./routes/routes');
 const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 4000;
 require('dotenv').config()
 
 const db = process.env.mongo_Url;
+
 // promise 
 mongoose.Promise = global.Promise;
 
 // connect data base
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err)=> {
-    if(!err){
-        console.log('connected to data base')
-    }else{
-        console.log('error to connect')
-    }
-});
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
+mongoose.connection
+  .once("open", function () {
+    console.log("DB Connected!");
+  })
+  .on("error", function (error) {
+    console.log("Error is: ", error);
+  });
+  
 
-
-
+app.use(express.json());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: 'false'})); 
+const routes = require('./routes/routes');
 app.use(routes);
 
 
