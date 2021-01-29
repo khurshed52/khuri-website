7956d19ca5f5ee974a949df47c77ad3a6644d23b
khurshed52 <khurshedkhan52@gmai.com>
2021-01-29 23:09:49 +0400

    footer copright date changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-declare var $:any
+declare var $:any;
+import * as moment from 'moment';
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  public footer:boolean = true;
+  today: string = moment().format('YYYY');
   constructor() { }
 
   ngOnInit() {
   }
 
   hideFooter() {
-    $('#footer').fadeOut();
+    // $('#footer').fadeOut();
+      this.footer = false
   }
 }
